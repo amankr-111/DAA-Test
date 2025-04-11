4413ee2c273d7028c4656b33cf0d4353c4fdc7a6
Ashutosh615singh@gmail.com <ashutosh615singh@gmail.com>
2025-04-11 23:11:02 +0530

    correcting login req

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -7,7 +7,7 @@ import { loginService } from "../model/authModel.js"
 
 export const login = async (req,res,next) => {
     try{
-  const result = loginService(req);
+  const result = await loginService(req);
   handleResponse(res,201,"User",result);
 }
 catch(e){
--- backend/src/model/authModel.js
+++ backend/src/model/authModel.js
@@ -2,5 +2,6 @@ import pool from '../config/db.js'
 
 
 export const loginService = async (req) =>{
-return JSON({username:req.body.email,password:req.body.password})
+
+return {"email": req.body.email}
 }
